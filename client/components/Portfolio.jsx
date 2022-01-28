a5259365fd2adc2eb801233ba894ead23bb68555
shayana-patel <shayana_patel@hotmail.com>
2022-01-28 19:21:57 +1300

    fix media query for about and portfolio height - footer in correct place -- responsive

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -9,22 +9,22 @@ const Portfolio = () => {
           <div className="row">
             <div className="col-sm-4">
               <img src="https://i.picsum.photos/id/398/360/360.jpg?hmac=F0XPMiqlGExUEWgJFVCCmiYekW_mMzj9vGuAlRCBt4c" className="img-responsive margin" alt="Placeholder 1" />
-              <h3>Project Title</h3>
-              <p>
+              <h3 className='project-title'>Project Title</h3>
+              <p className='project-description'>
                 Description of project - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </div>
             <div className="col-sm-4"> 
               <img src="https://i.picsum.photos/id/441/360/360.jpg?hmac=Wod9wD8dvCKaczLBNtPu68V3s6DPQd0_XgdBpyD35fY" className="img-responsive margin" alt="Placeholder 2" />
-              <h3>Project Title</h3>
-              <p>
+              <h3 className='project-title'>Project Title</h3>
+              <p className='project-description'>
                 Description of project - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </div>
             <div className="col-sm-4"> 
               <img src="https://i.picsum.photos/id/538/360/360.jpg?hmac=yR7o_bl9E0Z0pRKmSeMorzAfKvdqQfK9q0dQs1Enme8" className="img-responsive margin" alt="Placeholder 3" />
-              <h3>Project Title</h3>
-              <p>
+              <h3 className='project-title'>Project Title</h3>
+              <p className='project-description'>
                 Description of project - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </div>
