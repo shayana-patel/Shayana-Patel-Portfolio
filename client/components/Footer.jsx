df4ae2680c5a3a5c3c4e712454a02acb3d919f64
shayana-patel <shayana_patel@hotmail.com>
2022-01-28 18:52:33 +1300

    get footer to show at bottom of viewport window

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,12 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <footer id='footer' className="container-fluid bg-4 text-center">
+      <footer id='footer' className="container-fluid bg-4 text-center footer--pin">
         <p>Copywrite © 2022 SP</p> 
       </footer>
-      {/* <footer id='footer'>
-        <p>Copyright © 2022 SP</p>
-      </footer> */}
     </>
   )
 }
