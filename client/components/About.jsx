f371886d2f96b7fa48852bc4c0ea26fb4c5eb05e
shayana-patel <shayana_patel@hotmail.com>
2022-02-01 12:37:42 +1300

    fix spelling/ grammar error in about me description

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
           <img src="./server/public/shayana-profile.png" id='profile-img' className="img-fluid margin" alt="Shayana profile photo"/>
           <h3>Full Stack Software Developer</h3>
           <p>
-          I am a full stack software developer based in Wellington, New Zealand. I previously worked in the biomedical and laboratory science industry and decided to make a switch in to pursue a career in the tech industry. I have an enthusiasm for creating products that will have positive and valuable impact, and also have a great desire to learn about new technologies. My personal interests include hiking and spending time with family and friends.
+            I am a full stack software developer based in Wellington, New Zealand. I previously worked in the biomedical and laboratory science industry and decided to make a switch to pursue a career in the tech industry. I have an enthusiasm for creating products that will have positive and valuable impact, and also have a great desire to learn about new technologies. My personal interests include hiking and spending time with family and friends.
           </p>
         </div>
       </section>
