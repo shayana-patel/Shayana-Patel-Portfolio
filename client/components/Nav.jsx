d99865b6ad90946f7e73872476513e4aea98bb52
shayana-patel <shayana_patel@hotmail.com>
2022-01-31 18:56:23 +1300

    adjust accent colour for styling

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,61 +4,31 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <>
-    <nav className="navbar navbar-default navbar-fixed-top">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <Link to='/' className="navbar-brand">SHAYANA PATEL</Link>
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <Link to='/' className="navbar-brand">SHAYANA PATEL</Link>
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav">
+              <li>
+                <Link to="/about">ABOUT</Link>
+              </li>
+              <li>
+                <Link to="/portfolio">PORTFOLIO</Link>
+              </li>
+              <li>
+                <Link to="/contact">CONTACT</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="collapse navbar-collapse" id="myNavbar">
-          <ul className="nav navbar-nav">
-            {/* <li>
-              <Link to="/">HOME</Link>
-            </li> */}
-            <li>
-              <Link to="/about">ABOUT</Link>
-            </li>
-            <li>
-              <Link to="/portfolio">PORTFOLIO</Link>
-            </li>
-            <li>
-              <Link to="/contact">CONTACT</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-      {/* <nav id='navbar'className='navbar'>
-        <div className='logo'>
-          <h1>
-            <Link to='/'>SP</Link>
-          </h1>
-        </div>
-        <div>
-          <ul>
-            <li>
-              <Link to='/about' title='About Shayana'>
-                <h2>About</h2>
-              </Link>
-            </li>
-            <li>
-              <Link to='/portfolio' title="Shayana's Portfolio">
-                <h2>Portfolio</h2>
-              </Link>
-            </li>
-            <li>
-              <Link to='/contact' title='Contact Shayana'>
-                <h2>Contact</h2>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav> */}
+      </nav>
     </>
   )
 }
