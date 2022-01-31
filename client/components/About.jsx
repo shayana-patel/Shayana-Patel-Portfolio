ebd124921f92888d20b9622a5310080ca765c173
shayana-patel <shayana_patel@hotmail.com>
2022-01-31 17:29:57 +1300

    apply css to profile image - responsive based on screen size

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <section id='about'>
         <div className="container-fluid bg-1 text-center">
           <h1 className="margin section-title">Get to know me!</h1>
-          <img src="shayana-profile.png" id='profile-img'className="img-fluid margin" alt="Shayana profile photo"/>
+          <img src="shayana-profile.png" id='profile-img' className="img-fluid margin" alt="Shayana profile photo"/>
           <h3>Full Stack Software Developer</h3>
           <p>
           I am a full stack software developer based in Wellington, New Zealand. I previously worked in the biomedical and laboratory science industry and decided to make a switch in to pursue a career in the tech industry. I have an enthusiasm for creating products that will have positive and valuable impact, and also have a great desire to learn about new technologies. My personal interests include hiking and spending time with family and friends.
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -9,8 +9,7 @@ const Home = () => {
       <section id='home'>
         <div className="container-fluid bg-1 text-center">
           <h1>Shayana Patel</h1>
-          <h2>Full Stack Software Dveloper</h2>
-          <h2>Welcome to my website</h2>
+          <h2>Full Stack Software Developer</h2>
         </div>
       </section>
     </>
